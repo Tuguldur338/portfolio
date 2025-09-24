29a7a49f3e63013c5cad595233b8423926dc12c9
PADA-1-2-09 <PADA-1-2-09@Erxess-Mac-mini-4.local>
2025-09-24 20:34:56 +0800

    portfolio-update-3

--- components/header.tsx
+++ components/header.tsx
@@ -9,9 +9,9 @@ import Navbar from "react-bootstrap/Navbar";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 flex max-w-screen h-[100px] items-center justify-center bg-gray-100 fade-in1 z-[1000]">
-      <div className="w-[98%] flex justify-evenly">
-        <div className="flex items-center h-full gap-[10px]">
+    <div className="bg-gray-100">
+      <Navbar expand="lg" className="">
+        <Container>
           <Link href="/">
             <Image
               src="/images/orkhon-khasu-logo.png"
@@ -22,46 +22,45 @@ const Header = () => {
             />
           </Link>
 
-          <div className="flex gap-[5px]">
-            <h4>Orkhon</h4>
-            <h4>Khasu</h4>
-          </div>
-        </div>
+          <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
 
-        <div className="bg-gray-100">
-          <Navbar expand="lg" className="">
-            <Container>
-              <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <div className="flex">
+              <Nav className="ml-auto navbar-nav">
+                <Nav.Link
+                  href="/About"
+                  className="hover:!text-black !font-semibold"
+                >
+                  About
+                </Nav.Link>
 
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                  <Nav.Link
-                    href="/About"
-                    className="hover:!text-black !font-semibold"
-                  >
-                    About
-                  </Nav.Link>
+                <Nav.Link
+                  href="/Prices"
+                  className="hover:!text-black !font-semibold"
+                >
+                  Prices/fees
+                </Nav.Link>
 
-                  <Nav.Link
-                    href="/Prices"
-                    className="hover:!text-black !font-semibold"
-                  >
-                    Prices/fees
-                  </Nav.Link>
+                <Nav.Link
+                  href="/Teachers"
+                  className="hover:!text-black !font-semibold"
+                >
+                  Teachers
+                </Nav.Link>
+              </Nav>
 
-                  <Nav.Link
-                    href="/Teachers"
-                    className="hover:!text-black !font-semibold"
-                  >
-                    Teachers
-                  </Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-        </div>
-      </div>
-    </header>
+              <Nav>
+                <Nav.Link href="/"></Nav.Link>
+
+                <Nav.Link href="/"></Nav.Link>
+
+                <Nav.Link href="/"></Nav.Link>
+              </Nav>
+            </div>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   );
 };
 
--- components/main-content.tsx
+++ components/main-content.tsx
@@ -1,28 +1,90 @@
 "use client";
 
 import Image from "next/image";
+import { Button } from "react-bootstrap";
 import Carousel from "react-bootstrap/Carousel";
 
 const MainContent = () => {
   return (
     <div className="w-full">
-      <div className="mb-[100px]">
+      <div className="flex mb-[100px] justify-center">
         <h1 className="absolute left-[30%] text-black"></h1>
 
         <Image
-          src="/images/hero-image.jpg"
+          src="/images/orkhon-khasu-hero-image.avif"
           alt="src.name"
           width={500}
           height={500}
-          className="max-w-screen w-[100%] h-auto fade-in"
+          className="max-w-screen h-auto w-[100%] fade-in"
         />
       </div>
 
-      <div className="flex justify-center items-center">
-        <h1>Activities</h1>
+      <div className="flex flex-col items-center mb-[100px]">
+        <div>
+          <h1 className="font-serif">Three types of schools</h1>
+        </div>
+
+        <Carousel
+          className="w-full max-w-6xl mx-auto"
+          pause={false}
+          interval={3000}
+          fade
+        >
+          <Carousel.Item>
+            <div className="relative w-full h-64 sm:h-96 md:h-[500px] flex items-center justify-center bg-white">
+              <Image
+                src="/images/primary-school.avif"
+                alt="Saturday activity"
+                fill
+                className="rounded-lg object-cover object-center"
+              />
+              <div className="absolute inset-0 bg-black/50 rounded-lg"></div>
+            </div>
+
+            <Carousel.Caption className="bg-black/40 rounded p-2">
+              <h3>Primary school</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <div className="relative w-full h-64 sm:h-96 md:h-[500px] flex items-center justify-center bg-white">
+              <Image
+                src="/images/middle-school.avif"
+                alt="Saturday activity"
+                fill
+                className="rounded-lg object-cover object-center"
+              />
+              <div className="absolute inset-0 bg-black/50 rounded-lg"></div>
+            </div>
+
+            <Carousel.Caption className="bg-black/40 rounded p-2">
+              <h3>Middle school</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <div className="relative w-full h-64 sm:h-96 md:h-[500px] flex items-center justify-center bg-white">
+              <Image
+                src="/images/high-school.avif"
+                alt="Saturday activity"
+                fill
+                className="rounded-lg object-cover object-center"
+              />
+              <div className="absolute inset-0 bg-black/50 rounded-lg"></div>
+            </div>
+
+            <Carousel.Caption className="bg-black/40 rounded p-2">
+              <h3>Primary school</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
       </div>
 
-      <div className="flex gap-[5px]">
+      <div className="flex justify-center items-center">
+        <h1 className="font-serif">Activities</h1>
+      </div>
+
+      <div className="flex gap-[5px] mb-[100px]">
         <Carousel
           className="w-full max-w-6xl mx-auto"
           pause={false}
@@ -180,8 +242,35 @@ const MainContent = () => {
         </Carousel>
       </div>
 
-      <div>
-        <div></div>
+      <div className="flex h-[715px] flex-col bg-blue-400">
+        <div className="mx-auto mb-[50px]">
+          <h1 className="text-white font-sserif">What we achieved</h1>
+        </div>
+
+        <div className="flex items-end">
+          <Image
+            src="/images/what-we-achieved.avif"
+            alt="What we achieved"
+            width={500}
+            height={700}
+            className=""
+          />
+
+          <div className="bg-gray-300">
+            <div className=" h-[300px] flex items-center justify-center">
+              <h4 className="!text-blue-800">
+                Orkhon KhaSu School Basketball Team Wins Khan-Uul District
+                Championship for the 4th Consecutive Year! 🏆🏀
+              </h4>
+            </div>
+
+            <div className="flex justify-center items-center pb-[50px]">
+              <Button variant="warning" className="!rounded-[50px]">
+                Read more
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
